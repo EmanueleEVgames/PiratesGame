043fd5fed3562479b5c85a32a751a2f7bc0243f1
EmanueleEVgames <144163006+EmanueleEVgames@users.noreply.github.com>
2023-09-20 13:39:24 +0200

    fire effect
    aggiunto effetto sparo al cannone

--- PiratesGame/Assets/Scripts/CannonFireManager.cs
+++ PiratesGame/Assets/Scripts/CannonFireManager.cs
@@ -9,23 +9,40 @@ public class CannonFireManager : MonoBehaviour
     Animator anim;
     int numOfSecDelay = 2;
 
+    Animator animFireEffect;
+
     // Start is called before the first frame update
     void Start()
     {
-        lastTimeFire=DateTime.Now;
-        anim=GetComponent<Animator>();
+        lastTimeFire = DateTime.Now;
+        anim = GetComponent<Animator>();
         anim.SetBool("FireCannonBall", false);
+
+        Transform effect = GameObject.Find("FireEffect").transform;
+        foreach (Transform thisChild in effect)
+        {
+            animFireEffect = thisChild.GetComponent<Animator>();
+        }
+        //    animFireEffect = GetComponentInChildren<Animator>();
+
+        animFireEffect.SetBool("CanFire", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (DateTime.Now>=lastTimeFire.AddSeconds(numOfSecDelay))
+        if (DateTime.Now >= lastTimeFire.AddSeconds(numOfSecDelay))
         {
             lastTimeFire = DateTime.Now;
             anim.SetBool("FireCannonBall", true);
+            animFireEffect.SetBool("CanFire", true);
         }
         if (DateTime.Now >= lastTimeFire.AddSeconds(0.5))
+        {
             anim.SetBool("FireCannonBall", false);
+            animFireEffect.SetBool("CanFire", false);
+        }
+
+
     }
 }
